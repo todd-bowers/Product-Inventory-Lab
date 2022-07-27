d40c1b9ee83463efb7e95fc26ab1510ba342b50f
todd-bowers <toddbows@gmail.com>
2022-07-27 11:30:22 -0400

    added model characteristics

--- src/main/java/models/Wine.java
+++ src/main/java/models/Wine.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Wine {
+    private int id;
+    private String brand;
+    private String name;
+    private String type;
+    private int year;
+    private int qty;
+    private float price;
 }
