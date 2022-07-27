1e40c544bf5c842bc4d1a2f0cd2e22e2c4287828
todd-bowers <toddbows@gmail.com>
2022-07-27 13:36:37 -0400

    Wine testing, constructors, getters and setters done

--- src/main/java/models/Wine.java
+++ src/main/java/models/Wine.java
@@ -2,10 +2,67 @@ package models;
 
 public class Wine {
     private int id;
-    private String brand;
     private String name;
     private String type;
     private int year;
     private int qty;
     private float price;
+
+    public Wine() {
+    }
+    public Wine(int id, String name, String type, int year, int qty, float price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.year = year;
+        this.qty = qty;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/WineTest.java
+++ src/test/java/models/WineTest.java
@@ -1,4 +1,88 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class WineTest {
+
+    @Test
+    public void setIdTest() {
+        //Given
+        int expected = 2;
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setId(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getId());
+    }
+    @Test
+    public void setNameTest() {
+        //Given
+        String expected = "Francis Coppola";
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setName(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getName());
+    }
+    @Test
+    public void setTypeTest() {
+        //Given
+        String expected = "Pinot Noir";
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setType(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getType());
+    }
+    @Test
+    public void setYearTest() {
+        //Given
+        int expected = 2019;
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setYear(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getYear());
+    }
+    @Test
+    public void setQtyTest() {
+        //Given
+        int expected = 15;
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setQty(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getQty());
+    }
+    @Test
+    public void setPriceTest() {
+        //Given
+        float expected = 18;
+        //When
+        Wine testBottle = new Wine();
+        testBottle.setPrice(expected);
+        //Then
+        Assertions.assertEquals(expected, testBottle.getPrice());
+    }
+    @Test
+    public void constructorTest() {
+        //Given
+        int expectedID = 6;
+        String expectedName = "La Posta";
+        String expectedType = "Malbec";
+        int expectedYear = 2020;
+        int expectedQty = 20;
+        float expectedPrice = 20;
+        //When
+        Wine testBottle = new Wine(expectedID, expectedName, expectedType,
+                expectedYear, expectedQty, expectedPrice);
+        //Then
+        Assertions.assertEquals(expectedID, testBottle.getId());
+        Assertions.assertEquals(expectedName, testBottle.getName());
+        Assertions.assertEquals(expectedType, testBottle.getType());
+        Assertions.assertEquals(expectedYear, testBottle.getYear());
+        Assertions.assertEquals(expectedQty, testBottle.getQty());
+        Assertions.assertEquals(expectedPrice, testBottle.getPrice());
+    }
 }
