9c113748293d8528ebf664f21a8b94b7758d9f46
todd-bowers <toddbows@gmail.com>
2022-07-27 21:25:41 -0400

    SneakerService done

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,42 @@
 package services;
 
+import models.Sneaker;
+
+import java.util.ArrayList;
+
 public class SneakerService {
+    private static int nextId = 1;
+    private ArrayList<Sneaker> inventory = new ArrayList<>();
+
+    public Sneaker create(String brand, String model, String sport, double size, int qty, float price) {
+        Sneaker createdSneaker = new Sneaker(nextId++, brand, model, sport, size, qty, price);
+        inventory.add(createdSneaker);
+        return createdSneaker;
+    }
+
+    //should take an int and return an object with that id, if exists
+    public Sneaker findSneaker(int id) {
+        for (Sneaker sneaker : inventory) {
+            if (sneaker.getId() == id) return sneaker;
+        }
+        return null;
+    }
+
+    //should return a basic array copy of the ArrayList
+    public Sneaker[] findAll() {
+        Sneaker[] result = new Sneaker[inventory.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = inventory.get(i);
+        }
+        return result;
+    }
+
+    //should remove the object with this id from the ArrayList if it exists and return true
+    //otherwise return false
+    public boolean delete(int id) {
+        if (findSneaker(id) != null) {
+            inventory.remove(findSneaker(id));
+            return true;
+        } else return false;
+    }
 }
--- src/test/java/services/SneakerServiceTest.java
+++ src/test/java/services/SneakerServiceTest.java
@@ -1,4 +1,120 @@
 package services;
 
+import models.Sneaker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.sound.midi.Soundbank;
+
 public class SneakerServiceTest {
+    @Test
+    public void createTest() {
+        //Given
+        String expectedBrand = "Nike";
+        String expectedModel = "Dunk";
+        String expectedSport = "skateboarding";
+        double expectedSize = 11.5;
+        int expectedQty = 200;
+        float expectedPrice = 110;
+
+        //When
+        SneakerService sneakerService = new SneakerService();
+        Sneaker testSneaker = sneakerService.create(expectedBrand,expectedModel,
+                expectedSport, expectedSize, expectedQty, expectedPrice);
+
+        int actualId = testSneaker.getId();
+        String actualBrand = testSneaker.getBrand();
+        String actualModel = testSneaker.getModel();
+        String actualSport = testSneaker.getSport();
+        double actualSize = testSneaker.getSize();
+        int actualQty = testSneaker.getQty();
+        float actualPrice = testSneaker.getPrice();
+
+        //Then
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedModel, actualModel);
+        Assertions.assertEquals(expectedSport, actualSport);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+    @Test
+    public void findSneakerTest() {
+        //Given
+        String expectedBrand = "Nike";
+        String expectedModel = "Dunk";
+        String expectedSport = "skateboarding";
+        double expectedSize = 11.5;
+        int expectedQty = 200;
+        float expectedPrice = 110;
+        String expectedBrand2 = "Nike";
+        String expectedModel2 = "Vapormax";
+        String expectedSport2 = "lifestyle";
+        double expectedSize2 = 11.5;
+        int expectedQty2 = 100;
+        float expectedPrice2 = 210;
+        //When
+        SneakerService sneakerService = new SneakerService();
+        Sneaker expectedSneaker = sneakerService.create(expectedBrand,expectedModel,
+                expectedSport, expectedSize, expectedQty, expectedPrice);
+        Sneaker expectedSneaker2 = sneakerService.create(expectedBrand2,expectedModel2,
+                expectedSport2, expectedSize2, expectedQty2, expectedPrice2);
+        Sneaker actualSneaker = sneakerService.findSneaker(expectedSneaker.getId());
+        Sneaker actualSneaker2 = sneakerService.findSneaker(expectedSneaker2.getId());
+        //Then
+        Assertions.assertEquals(expectedSneaker, actualSneaker);
+        Assertions.assertEquals(expectedSneaker2, actualSneaker2);
+    }
+    @Test
+    public void findAllTest() {
+        //Given
+        String expectedBrand = "Nike";
+        String expectedModel = "Dunk";
+        String expectedSport = "skateboarding";
+        double expectedSize = 11.5;
+        int expectedQty = 200;
+        float expectedPrice = 110;
+        String expectedBrand2 = "Nike";
+        String expectedModel2 = "Vapormax";
+        String expectedSport2 = "lifestyle";
+        double expectedSize2 = 11.5;
+        int expectedQty2 = 100;
+        float expectedPrice2 = 210;
+        //When
+        SneakerService sneakerService = new SneakerService();
+        Sneaker expectedSneaker = sneakerService.create(expectedBrand,expectedModel,
+                expectedSport, expectedSize, expectedQty, expectedPrice);
+        Sneaker expectedSneaker2 = sneakerService.create(expectedBrand2,expectedModel2,
+                expectedSport2, expectedSize2, expectedQty2, expectedPrice2);
+        Sneaker[] expected = {expectedSneaker, expectedSneaker2};
+        Sneaker[] actual = sneakerService.findAll();
+        //Then
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void deleteTest2() {
+        //Given
+        String expectedBrand = "Nike";
+        String expectedModel = "Dunk";
+        String expectedSport = "skateboarding";
+        double expectedSize = 11.5;
+        int expectedQty = 200;
+        float expectedPrice = 110;
+        String expectedBrand2 = "Nike";
+        String expectedModel2 = "Vapormax";
+        String expectedSport2 = "lifestyle";
+        double expectedSize2 = 11.5;
+        int expectedQty2 = 100;
+        float expectedPrice2 = 210;
+        //When
+        SneakerService sneakerService = new SneakerService();
+        Sneaker expectedSneaker = sneakerService.create(expectedBrand,expectedModel,
+                expectedSport, expectedSize, expectedQty, expectedPrice);
+        Sneaker expectedSneaker2 = sneakerService.create(expectedBrand2,expectedModel2,
+                expectedSport2, expectedSize2, expectedQty2, expectedPrice2);
+
+        //Then
+        Assertions.assertTrue(sneakerService.delete(1));
+    }
 }
